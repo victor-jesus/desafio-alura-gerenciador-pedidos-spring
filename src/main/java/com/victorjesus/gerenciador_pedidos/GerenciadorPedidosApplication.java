5f4949c232a4041c3dc70fb9daf047b12a222b56
victor-jesus <victorgustavo191@gmail.com>
2025-07-15 22:15:03 -0300

    first commit

---
+++ src/main/java/com/victorjesus/gerenciador_pedidos/GerenciadorPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.victorjesus.gerenciador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorjesus/gerenciador_pedidos/GerenciadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorjesus.gerenciador_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
